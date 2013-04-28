fc55035dcd9a8ea5ab90f9a586b0c7f601196126
ada.young <cng1985@gmail.com>
2013-04-28 08:50:14 +0800

    增加注释

--- src/com/callmewill/launcher2/MainActivity.java
+++ src/com/callmewill/launcher2/MainActivity.java
@@ -1,10 +1,13 @@
 package com.callmewill.launcher2;
+import android.animation.TimeInterpolator;
 import android.animation.ValueAnimator;
 import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+
 import com.callmewill.launcher2.widget.DemoWidget;
 public class MainActivity extends Activity {
 
@@ -14,13 +17,13 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		demoWidget=(DemoWidget) findViewById(R.id.demoWidget);
 		
-		handler.postDelayed(new Runnable() {
-			
-			@Override
-			public void run() {
-				demoWidget.setTranslationX(800);
-			}
-		}, 2000);
+//		handler.postDelayed(new Runnable() {
+//			
+//			@Override
+//			public void run() {
+//				demoWidget.setTranslationX(800);
+//			}
+//		}, 2000);
 	}
 	private Handler handler=new Handler();
 	DemoWidget demoWidget;
@@ -29,6 +32,7 @@ public class MainActivity extends Activity {
 		ValueAnimator animator=new ValueAnimator();
 		animator.setFloatValues(5);
 		animator.setDuration(5000);
+		animator.setInterpolator(new LinearInterpolator());
 		animator.addUpdateListener(new AnimatorUpdateListener() {
 			
 			@Override
--- src/com/callmewill/launcher2/widget/AppsCustomizePagedView.java
+++ src/com/callmewill/launcher2/widget/AppsCustomizePagedView.java
@@ -1124,7 +1124,11 @@ public class AppsCustomizePagedView extends PagedViewWithDraggableItems implemen
         layout.measure(widthSpec, heightSpec);
         setVisibilityOnChildren(layout, View.VISIBLE);
     }
-
+   /**
+    * 同步应用程序
+    * @param page
+    * @param immediate
+    */
     public void syncAppsPageItems(int page, boolean immediate) {
         // ensure that we have the right number of items on the pages
         int numCells = mCellCountX * mCellCountY;
@@ -1639,6 +1643,7 @@ public class AppsCustomizePagedView extends PagedViewWithDraggableItems implemen
         cancelAllTasks();
 
         Context context = getContext();
+
         for (int j = 0; j < mNumWidgetPages; ++j) {
             PagedViewGridLayout layout = new PagedViewGridLayout(context, mWidgetCountX,
                     mWidgetCountY);
@@ -1646,12 +1651,14 @@ public class AppsCustomizePagedView extends PagedViewWithDraggableItems implemen
             addView(layout, new PagedView.LayoutParams(LayoutParams.MATCH_PARENT,
                     LayoutParams.MATCH_PARENT));
         }
-
+        
         for (int i = 0; i < mNumAppsPages; ++i) {
             PagedViewCellLayout layout = new PagedViewCellLayout(context);
             setupPage(layout);
             addView(layout);
         }
+        
+      
     }
 
     @Override
